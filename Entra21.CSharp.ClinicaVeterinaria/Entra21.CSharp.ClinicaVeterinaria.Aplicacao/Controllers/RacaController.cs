adb9a8827efc166f34c03107fa631191a4b4b044
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-17 20:40:52 -0300

    Atualização

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -46,9 +46,9 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             return View();
         }
 
-        [Route("/raca/registrar")]
+        [Route("/raca/Cadastrar")]
         [HttpPost]
-        public IActionResult Registrar (
+        public IActionResult Cadastrar (
             [FromForm] string nome,
             [FromForm] string especie)
         {
@@ -80,14 +80,15 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             return View("Editar");
         }
 
-        [Route("/raca/alterar")]
+        [Route("/raca/editar")]
         [HttpPost]
-        public IActionResult Alterar(
+        
+        public IActionResult Editar(
             [FromForm] int id,
             [FromForm] string nome,
             [FromForm] string especie)
         {
-            _racaServico.Alterar(id, nome, especie);
+            _racaServico.Editar(id, nome, especie);
 
             return RedirectToAction("Index");
         }
