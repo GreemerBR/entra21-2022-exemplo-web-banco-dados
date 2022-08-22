336e28dbf65eed478e11baaa1164b061c0ab4dac
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-22 19:32:28 -0300

    Alterações para o Migration

--- Entra21.CSharp.ClinicaVeterinaria/Repositorio/Entidades/Raca.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/Entidades/Raca.cs
@@ -3,6 +3,7 @@
     // Entidades: são representações das tabelas para classe de objetos
     public class Raca
     {
+        // 1ª etapa migration: criar a entidade
         public int Id { get; set; }
 
         public string Nome { get; set; }
